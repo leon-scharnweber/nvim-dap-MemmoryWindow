d4635ac5ecf3efd01533d93b96d2fbe04e72948c
Leon Scharnwebwer <leon.scharnweber@gmail.com>
2026-07-30 13:19:14 +0200

    unötige user cmd entfernt

--- plugin/createUserCommands.lua
+++ plugin/createUserCommands.lua
@@ -1,23 +1,3 @@
-vim.api.nvim_create_user_command("Setup", function()
-	require("memoryWatchWindow").setup()
-end, {})
-
-vim.api.nvim_create_user_command("ReadMemoryAddr", function(opts)
-	require("memoryWatchWindow").readMemoryAddr(opts.fargs[1], tonumber(opts.fargs[2]))
-end, { nargs = "+" })
-
-vim.api.nvim_create_user_command("ReadMemoryVar", function(opts)
-	require("memoryWatchWindow").readMemoryVar(opts.fargs[1])
-end, { nargs = 1 })
-
-vim.api.nvim_create_user_command("WriteMemory", function(opts)
-	require("memoryWatchWindow").writeMemory(opts.fargs[1], opts.fargs[2])
-end, { nargs = "+" })
-
-vim.api.nvim_create_user_command("ReadMemory", function(opts)
-	require("memoryWatchWindow").readMemory(opts.fargs[1])
-end, { nargs = 1 })
-
 vim.api.nvim_create_user_command("ChangeAddr", function(opts)
 	require("memoryWatchWindow").changeCurrAddr(opts.fargs[1])
 end, { nargs = 1 })
